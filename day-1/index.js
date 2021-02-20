58aa983d1afd384977930d5bd1809aa4c1892769
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 13:11:06 +0300

    Add prettier config file

--- day-1/index.js
+++ day-1/index.js
@@ -1,19 +1,30 @@
-const oddNumbers = [1, 3, 5, 7, 9, 11, 13, 17];
+const oddNumbers = [1, 3, 5, 7, 9, 11, 13, 17]
 
 // Filter:
-const biggerThenSix = oddNumbers.filter((number) => number > 6);
-console.table(biggerThenSix);
+const biggerThenSix = oddNumbers.filter((number) => number > 6)
+console.table(biggerThenSix)
 
 //Truthy🤞
-if ({}) console.log("yey");
+if ({}) console.log('yey')
 
+// Usage of filters
 const students = [
-  { name: "Quincy", grade: 96 },
-  { name: "Jason", grade: 84 },
-  { name: "Alexis", grade: 100 },
-  { name: "Sam", grade: 65 },
-  { name: "Katie", grade: 90 },
-];
+  { name: 'Quincy', grade: 96 },
+  { name: 'Jason', grade: 84 },
+  { name: 'Alexis', grade: 100 },
+  { name: 'Sam', grade: 65 },
+  { name: 'Katie', grade: 90 },
+]
 
-const yey = students.filter((student) => student.grade > 90);
-console.log(yey);
+const yey = students.filter((student) => student.grade > 90)
+console.log(yey)
+
+const players = [
+  { name: 'Lebron', rating: 98, team: 'Lakers' },
+  { name: '', rating: 98, team: 'Lakers' },
+  { name: '', rating: 98 },
+  { name: '', rating: 98 },
+]
+
+const team = players.filter((player) => player.hasOwnProperty('team'))
+console.log(team)
