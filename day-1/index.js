e4119e11f3c4fa5172dbdafdd7b384770e2f92d3
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 13:00:23 +0300

    Add filter function something

--- day-1/index.js
+++ day-1/index.js
@@ -6,3 +6,14 @@ console.table(biggerThenSix);
 
 //Truthy🤞
 if ({}) console.log("yey");
+
+const students = [
+  { name: "Quincy", grade: 96 },
+  { name: "Jason", grade: 84 },
+  { name: "Alexis", grade: 100 },
+  { name: "Sam", grade: 65 },
+  { name: "Katie", grade: 90 },
+];
+
+const yey = students.filter((student) => student.grade > 90);
+console.log(yey);
