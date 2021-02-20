4ab2474d35b843beed0d516e4f7ddf32cffca2c7
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 12:56:23 +0300

    Inıtial commit for day-1

---
+++ day-1/index.js
@@ -0,0 +1,8 @@
+const oddNumbers = [1, 3, 5, 7, 9, 11, 13, 17];
+
+// Filter:
+const biggerThenSix = oddNumbers.filter((number) => number > 6);
+console.table(biggerThenSix);
+
+//Truthy🤞
+if ({}) console.log("yey");
