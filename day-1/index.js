49d5cc2b2d756150507b48e341c158d52f563ae4
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 13:57:06 +0300

    Add reduce function for object of arrays

--- day-1/index.js
+++ day-1/index.js
@@ -1,11 +1,11 @@
-// const oddNumbers = [1, 3, 5, 7, 9, 11, 13, 17]
+// const oddNumbers = [1, 3, 5, 7, 9, 11, 13, 17];
 
 // // Filter:
-// const biggerThenSix = oddNumbers.filter((number) => number > 6)
-// console.table(biggerThenSix)
+// const biggerThenSix = oddNumbers.filter((number) => number > 6);
+// console.table(biggerThenSix);
 
 // //Truthy🤞
-// if ({}) console.log('yey')
+// if ({}) console.log('yey');
 
 // // Usage of filters
 // const students = [
@@ -14,19 +14,36 @@
 //   { name: 'Alexis', grade: 100 },
 //   { name: 'Sam', grade: 65 },
 //   { name: 'Katie', grade: 90 },
-// ]
+// ];
 
-// const yey = students.filter((student) => student.grade > 90)
-// console.log(yey)
+// const yey = students.filter((student) => student.grade > 90);
+// console.log(yey);
 
-// Filter with object filltering
+// //Filter with object filltering
 
 // const players = [
 //   { name: 'Lebron', rating: 98, team: 'Lakers' },
 //   { name: '', rating: 98, team: 'Lakers' },
 //   { name: '', rating: 98 },
 //   { name: '', rating: 98 },
-// ]
+// ];
 
-// const team = players.filter((player) => player.hasOwnProperty('team'))
-// console.log(team)
+// const team = players.filter((player) => player.hasOwnProperty('team'));
+// console.log(team);
+
+// //Reduce :
+// //With nums
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const sum = numbers.reduce((acc, value) => acc + value, 0);
+// console.log(sum);
+
+// // Reduce with array of objects
+// const shopItems = [
+//   { description: 'Eggs (Dosen)', quantity: 0.5, total: 3 },
+//   { description: 'Chese', quantity: 0.5, total: 3 },
+//   { description: 'Butter', quantity: 0.5, total: 3 },
+// ];
+
+// const total = shopItems.reduce((acc, value) => acc + value.total, 0);
+
+// console.log(total);
