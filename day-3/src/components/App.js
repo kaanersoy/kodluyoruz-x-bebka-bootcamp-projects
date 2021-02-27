da041898ac0414c249e8b3937151f536af10d71e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-27 18:57:03 +0300

    Update the main folder schema

---
+++ day-3/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-content">
+      <h1>Frenzify</h1>
+      <p>Click and watch how rises your friend count!</p>
+    </div>
+  );
+}
+
+export default App;
