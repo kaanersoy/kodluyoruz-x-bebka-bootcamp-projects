d5d99adb909a7cd512253cef96c5375c599185e1
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-27 19:28:11 +0300

    Add some styles and main page cover

--- day-3/src/components/App.js
+++ day-3/src/components/App.js
@@ -1,11 +1,21 @@
 import './App.scss';
+import Header from '../components/Header/Header';
+import mainPageCoverImage from '../images/main-page-cover.svg';
 
 function App() {
   return (
-    <div className="app-content">
-      <h1>Frenzify</h1>
-      <p>Click and watch how rises your friend count!</p>
-    </div>
+    <section>
+      <Header></Header>
+      <div className="app-content">
+        <div className="content">
+          <h1>Frienzify</h1>
+          <p>Click and watch how rises your friend count!</p>
+        </div>
+        <div className="image">
+          <img src={mainPageCoverImage} alt="" />
+        </div>
+      </div>
+    </section>
   );
 }
 
--- day-3/src/components/Header/Header.js
+++ day-3/src/components/Header/Header.js
@@ -2,5 +2,16 @@ import React from 'react';
 import './Header.scss';
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <div className="logo">
+        <h1>Frenzify</h1>
+      </div>
+      <div className="urls">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/friends">Friends</a>
+      </div>
+    </header>
+  );
 }
