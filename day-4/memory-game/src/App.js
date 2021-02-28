dd7d4b90502638292a2722b7ab13b118c67a4775
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-28 22:47:35 +0300

    Add flipped cards

--- day-4/memory-game/src/App.js
+++ day-4/memory-game/src/App.js
@@ -1,5 +1,26 @@
+import './App.scss';
+
 function App() {
-  return <div className="App"></div>;
+  const emojies = ['💩', '😱', '👶', '💖'];
+  return (
+    <div className="App">
+      <header>
+        <h1>CARD GAME</h1>
+      </header>
+      <div className="card-container">
+        {emojies.map((emoji) => (
+          <div className="card">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">CARD</div>
+              <div class="flip-card-back">
+                <span>{emoji}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
