f1c4f45074fa1c387fa3d23008f769e7ab0f4ba2
Kaan Ersoy <kaanersoy12@gmail.com>
2021-03-01 00:04:24 +0300

    Update for selected cards

--- day-4/memory-game/src/App.js
+++ day-4/memory-game/src/App.js
@@ -1,23 +1,56 @@
 import './App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const emojies = ['💩', '😱', '👶', '💖'];
+  const [shuffledCards, setShuffledCards] = useState(false);
+  const [selectedCard, setSeletedCard] = useState(false);
+  const [matches, setMatches] = useState([]);
+
+  function selectCard(item) {
+    if (selectCard) {
+      if (selectedCard === item) {
+        return setMatches([...matches, item]);
+      }
+    }
+    return setSeletedCard(item);
+  }
+
+  useEffect(() => {
+    function shuffleArray(array) {
+      array.sort(() => Math.random() - 0.5);
+    }
+    const emojies = ['💩', '😱', '👶', '💖'];
+    shuffleArray(emojies);
+    const firstPattern = emojies;
+    shuffleArray(emojies);
+    const secondPattern = emojies;
+    setShuffledCards([...secondPattern, ...secondPattern]);
+  }, []);
+
   return (
     <div className="App">
+      <div className="login-screen"></div>
       <header>
         <h1>CARD GAME</h1>
       </header>
       <div className="card-container">
-        {emojies.map((emoji) => (
-          <div className="card">
-            <div class="flip-card-inner">
-              <div class="flip-card-front">CARD</div>
-              <div class="flip-card-back">
-                <span>{emoji}</span>
+        {shuffledCards
+          ? shuffledCards.map((emoji) => (
+              <div
+                className="card"
+                onClick={() => {
+                  selectCard(emoji);
+                }}
+              >
+                <div class="flip-card-inner">
+                  <div class="flip-card-front"></div>
+                  <div class="flip-card-back">
+                    <span>{emoji}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        ))}
+            ))
+          : null}
       </div>
     </div>
   );
